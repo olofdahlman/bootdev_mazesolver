1c4a9ac3592c852985fa63a22656d3ffb6376197
olofdahlman <olofdahlman@live.se>
2025-03-05 23:02:13 +0100

    4: Added point and line classes, added draw_line to window class and updated main with a simple static line test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Main codeblock for bootdev guided maze solver project - for now used to test push/pull functionality and tkinter functionality
+# Main codeblock for bootdev guided maze solver project
+# With the help of the boots AI, I managed to expand the sources for ubuntu dev packages to properly retrieve and install tcl, tk and lzma packages
+# The sources are potentially drawn from non-open source libraries though - I do not believe they are, but I cannot be 100% certain
+# This main file tkinter test does work but the tktiner_venv intrepreter was built during a previous tkinter test before the install issues were solved
+from src.classes import *
+win = Window(800, 600)
+point1 = Point(1, 1)
+point2 = Point(300, 300)
+line = Line(point1, point2)  
+win.draw_line(line, "red")  #These lines create a simple red line as a test
+win.wait_for_close()
+
--- src/classes.py
+++ src/classes.py
@@ -25,3 +25,20 @@ class Window:
     def close(self):
         self.__running = False
         
+    def draw_line(self, Line, fill_colour):
+        Line.draw(self.__canvas, fill_colour)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, Canvas, fill_colour):
+        Canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_colour, width=2)
+
