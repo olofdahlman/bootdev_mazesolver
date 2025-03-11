e4bd000d2cb63f5def1986b3f3deeaaf703cb462
olofdahlman <olofdahlman@live.se>
2025-03-11 23:53:48 +0100

    9.1: Updated readme to finalize project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bootdev_mazesolver
-# A maze solver program written as a part of a guided project - under construction
+# A maze solver program written as a part of a guided project
+# Code within generates a small 2-dimensional maze in a specific rectangular shape - does not support more exotic types
+# Also contains a solver function that finds the path through the maze and draws a red line as it does so
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Main codeblock for bootdev guided maze solver project
 # With the help of the boots AI, I managed to expand the sources for ubuntu dev packages to properly retrieve and install tcl, tk and lzma packages
 # The sources are potentially drawn from non-open source libraries though - I do not believe they are, but I cannot be 100% certain
-# This main file tkinter test does work but the tktiner_venv intrepreter was built during a previous tkinter test before the install issues were solved
+# Alter the parameters of the maze as desired, all aspects of creation are contained within simply creating a maze class object
+# You need to run the maze.solve() function to solve it and create the solution line, this is not done in maze class creation
 from src.classes import *
 
 def main():
@@ -15,7 +16,11 @@ def main():
     win = Window(screen_x, screen_y)
 
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
-
+    result = maze.solve()
+    if result:
+        print("Exit found")
+    else:
+        print("Exit not found")
     win.wait_for_close()
 
 
