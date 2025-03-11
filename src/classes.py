c2bae6a34b905ca1d7411e8a909b2a9c819c4003
olofdahlman <olofdahlman@live.se>
2025-03-11 01:58:48 +0100

    8: Added many comments, may also have to change strategy for the maze solver part.

--- src/classes.py
+++ src/classes.py
@@ -48,6 +48,11 @@ class Line:
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
+    
+    def draw_center_line(self, canvas, fill_color="blue"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
 
 
 class Cell:
@@ -167,17 +172,17 @@ class Maze:
         self._win.redraw()
         time.sleep(0.025)
 
-    def _break_entrance_and_exit(self):
+    def _break_entrance_and_exit(self):     #Opens the entrance and exit walls
         self._cells[0][0].has_top_wall = False
         self._draw_cell(0, 0)
 
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
 
-    def _break_walls_r(self, i, j):
+    def _break_walls_r(self, i, j):         #Creates the path through the maze
         self._cells[i][j].visited = True
 
-        while True:
+        while True:                #This section checks if the prospective next cell is within bounds and if it hasen't been visited for all neighbours
             adjacent_cells = []
             if self._num_cols - 1 > i:
                 if self._cells[i + 1][j].visited == False:
@@ -192,14 +197,14 @@ class Maze:
                 if self._cells[i][j - 1].visited == False:
                     adjacent_cells.append([i, j - 1])
 
-            if not adjacent_cells:
+            if not adjacent_cells:          #The cell lists updates every iteration and if there are none to add, this cell eliminates itself
                 self._draw_cell(i, j)
                 return
 
             random_num = random.randrange(len(adjacent_cells))
-            random_dir = adjacent_cells[random_num]
+            random_dir = adjacent_cells[random_num]     #Randomly select an adjacent cell to visit
 
-            if i + 1 == random_dir[0]:
+            if i + 1 == random_dir[0]:              #depending on which neighbour was visited, destroy the wall separating them
                 self._cells[i][j].has_right_wall = False
                 self._cells[i + 1][j].has_left_wall = False
             if i - 1 == random_dir[0]:
@@ -212,12 +217,64 @@ class Maze:
                 self._cells[i][j].has_top_wall = False
                 self._cells[i][j - 1].has_bottom_wall = False
 
-            self._break_walls_r(random_dir[0], random_dir[1])
+            self._break_walls_r(random_dir[0], random_dir[1])       #Recursively call the method untill all cells have been visited, which ends the method call
                 
     def _reset_cells_visited(self):
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._cells[i][j].visited = False
+
+    def solve(self):
+        i = 0
+        j = 0
+        self._solve_r(i, j)
+        if self._solve_path_found:
+            path_solved = []
+            path_solved.append([self._num_cols, self._num_rows])    #Immediately append the maze exit - has to be found if solve_path_found is True
+            while self._solve_path:
+                for cell in self._solve_path:   #This part is intended to iterate over the dictionary untill it finds the preceeding cell
+                    a = cell[:1]                #This will not work because the path can run right next to itself while still having a wall - add a wall-checking part
+                    b = cell[1:]
+                    if (a + 1 == path_solved[-1][0] or a - 1 == path_solved[-1][0]) ^ (b + 1 == path_solved[-1][1] or b - 1 == path_solved[-1][1]):
+                        path_solved.append(self._solve_path[cell])
+                        del self._solve_path[cell]
+            
+
+
+        else:
+            return False
+        
+
+    def _solve_r(self, i, j):       #This function should use the method-specific variables path_found and path to log the path travelled and if the end has been found
+        self._cells[i][j].visited = True
+        if i == self._num_cols and j == self._num_rows:
+            self._solve_path_found = True       #If the end path is found, this will ensure the program stops checking and the path can processed in solve method
+
+        while not self._solve_path_found:
+            adjacent_cells = []
+            string = f"{i}" + f"{j}"    #Creating a unique key id from the two integers - due to the maze setup, they SHOULD be unique
+            self._solve_path.update({string : [i, j]})  #Use the unique key to store the corresponding cell list coordinate - the key allows them to be sorted later
+            if self._num_cols - 1 > i:                     #Could potentially just create a mirrored maze list by creating new cell class objects?
+                if self._cells[i + 1][j].visited == False and self._cells[i][j].has_right_wall == False:
+                    adjacent_cells.append([i + 1, j])
+            if 0 < i:
+                if self._cells[i - 1][j].visited == False and self._cells[i][j].has_left_wall == False:
+                    adjacent_cells.append([i - 1, j])
+            if self._num_rows - 1 > j:
+                if self._cells[i][j + 1].visited == False and self._cells[i][j].has_top_wall == False:
+                    adjacent_cells.append([i, j + 1])
+            if 0 < j:
+                if self._cells[i][j - 1].visited == False and self._cells[i][j].has_bottom_wall == False:
+                    adjacent_cells.append([i, j - 1])
+
+            if not adjacent_cells:      #Designed so that dead-end cells self-filter themselves out of the final path
+                del self._solve_path[string]
+                
+                return
+            
+            random_num = random.randrange(len(adjacent_cells))
+            random_dir = adjacent_cells[random_num]
+            self._solve_r(random_dir[0], random_dir[1])     #Same random cell selection and recursive method call as in the wall breaking method
 
 
 
